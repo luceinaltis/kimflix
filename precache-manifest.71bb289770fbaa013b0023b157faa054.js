68c466af1f1fa9950c07d81881a04411eae3d6d6
Ki Hwan <luceinaltis2020@naver.com>
2020-08-26 16:56:59 +0900

    Updates

---
+++ precache-manifest.71bb289770fbaa013b0023b157faa054.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e4f2a36d74f0b70f0c07988d49cbe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e104c2f4bc93f2c486",
+    "url": "/static/js/2.372fc453.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.372fc453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4c7dac27d801565c21",
+    "url": "/static/js/main.c5f03fae.chunk.js"
+  },
+  {
+    "revision": "ad19d7be220c694beb3b",
+    "url": "/static/js/runtime-main.258df3c8.js"
+  },
+  {
+    "revision": "fc7ece8e8ee1f5db97577a4622f33975",
+    "url": "/static/media/noPoster.fc7ece8e.jpg"
+  }
+]);
