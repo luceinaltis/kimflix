b202fc652f81afa07633f1c562727b7a02e821da
Ki Hwan <luceinaltis2020@naver.com>
2020-08-26 17:07:23 +0900

    Updates

---
+++ precache-manifest.675c54884d857b03e8596901153f0723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11977464d6b2f4e439c93df00a12412d",
+    "url": "/kimflix/index.html"
+  },
+  {
+    "revision": "2e8a2910805f89718420",
+    "url": "/kimflix/static/js/2.96d959f7.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/kimflix/static/js/2.96d959f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e468e3368a007a93271d",
+    "url": "/kimflix/static/js/main.964c402b.chunk.js"
+  },
+  {
+    "revision": "e7f516fc310820bccf00",
+    "url": "/kimflix/static/js/runtime-main.4a13dceb.js"
+  },
+  {
+    "revision": "fc7ece8e8ee1f5db97577a4622f33975",
+    "url": "/kimflix/static/media/noPoster.fc7ece8e.jpg"
+  }
+]);
